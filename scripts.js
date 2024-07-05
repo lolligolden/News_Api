1b44627d3c77b5655c3ee906a2c926593d7d6ba2
lolligolden <lolligolden@github.com>
2024-07-04 20:33:35 -0500

    Setting up News App

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const apiKey = process.env.NEWS_API_KEY;
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+async function fetchNews() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        //TODO: Add a function call to display the news
+    } catch (error) {
+        console.error('There was an error!', error);
+    }
+}
+
+fetchNews();
+
+function displayNews(articles) {
+    const newsDiv = document.querySelector('#news');
+    for(const article of articles) {
+        const articleDiv = documen.createElement('div');
+
+        //create and append a headline to the article Div
+        const title = document.createElement('h4');
+        title.textContent = article.title;
+        articleDiv.appendChild(title);
+
+    }
+}
